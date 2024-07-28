24cf9f2b5991de0ed8b895842a4fb64703b09ce9
Paweł Kołodziejski <aquadran@gmail.com>
2024-07-28 19:12:02 +0200

    added omap drm OSD support

--- src/video/out/vo_drm_omap.c
+++ src/video/out/vo_drm_omap.c
@@ -30,6 +30,7 @@
 #include "drm_common.h"
 #include "osdep/timer.h"
 #include "sub/osd.h"
+#include "sub/draw_bmp.h"
 #include "video/fmt-conversion.h"
 #include "video/mp_image.h"
 #include "video/out/present_sync.h"
@@ -56,12 +57,15 @@ struct priv {
     struct mp_rect src;
     struct mp_rect dst;
     struct mp_osd_res osd;
+    struct mp_draw_sub_cache *osd_cache;
     struct mp_sws_context *sws;
 
     struct framebuffer **bufs;
     struct framebuffer *primary_buf;
+    struct framebuffer *osd_buf;
     int front_buf;
     int buf_count;
+    bool osd_changed;
 };
 
 static void destroy_framebuffer(struct framebuffer *fb)
@@ -199,6 +203,15 @@ static int reconfig(struct vo *vo, struct mp_image_params *params)
     vo->dwidth = params->w;
     vo->dheight = params->h;
     vo_get_src_dst_rects(vo, &p->src, &p->dst, &p->osd);
+    p->osd = (struct mp_osd_res) {
+        .w = drm->fb->width / 2,
+        .h = drm->fb->height / 2,
+        .mt = 0,
+        .mb = 0,
+        .ml = 0,
+        .mr = 0,
+        .display_par = 1,
+    };
 
     struct mp_imgfmt_desc fmt = mp_imgfmt_get_desc(p->imgfmt);
     p->dst.x0 = MP_ALIGN_DOWN(p->dst.x0, fmt.align_x);
@@ -243,6 +256,8 @@ static int reconfig(struct vo *vo, struct mp_image_params *params)
     }
 
     vo->want_redraw = true;
+    p->osd_changed = false;
+
     return 0;
 }
 
@@ -286,6 +301,8 @@ static void draw_frame(struct vo *vo, struct vo_frame *frame)
     struct vo_drm_state *drm = vo->drm;
     struct priv *p = vo->priv;
     struct framebuffer *fb;
+    struct mp_rect act_rc[1], mod_rc[64];
+    int num_act_rc = 0, num_mod_rc = 0;
 
     if (!drm->active)
         return;
@@ -315,10 +332,8 @@ static void draw_frame(struct vo *vo, struct vo_frame *frame)
                 mp_image_crop_rc(&src, src_rc);
 
                 mp_sws_scale(p->sws, p->cur_frame_cropped, &src);
-                //osd_draw_on_image(vo->osd, p->osd, src.pts, 0, p->cur_frame);
             } else {
                 mp_image_clear(p->cur_frame, 0, 0, p->cur_frame->w, p->cur_frame->h);
-                //osd_draw_on_image(vo->osd, p->osd, 0, 0, p->cur_frame);
             }
 
             memcpy_pic(fb->map, p->cur_frame->planes[0],
@@ -333,6 +348,39 @@ static void draw_frame(struct vo *vo, struct vo_frame *frame)
             if (mpi != p->last_input) {
                 talloc_free(p->last_input);
                 p->last_input = mpi;
+            }
+        }
+    }
+
+    if (!p->osd_cache)
+        p->osd_cache = mp_draw_sub_alloc(p, vo->global);
+
+    struct sub_bitmap_list *sbs = osd_render(vo->osd, p->osd, frame->current->pts, 0, mp_draw_sub_formats);
+    if (!sbs) {
+        MP_ERR(vo, "Cannot get sub bitmap list!\n");
+    } else {
+        struct mp_image *osd_sub = mp_draw_sub_overlay(p->osd_cache, sbs,
+                                                       act_rc, MP_ARRAY_SIZE(act_rc), &num_act_rc,
+                                                       mod_rc, MP_ARRAY_SIZE(mod_rc), &num_mod_rc);
+        talloc_free(sbs);
+        if (!osd_sub) {
+            MP_ERR(vo, "Cannot get OSD image!\n");
+        } else {
+            for (int n = 0; n < num_mod_rc; n++) {
+                 p->osd_changed = true;
+                 struct mp_rect rc = mod_rc[n];
+
+                 int rw = mp_rect_w(rc);
+                 int rh = mp_rect_h(rc);
+
+                 void *src = mp_image_pixel_ptr(osd_sub, 0, rc.x0, rc.y0);
+                 void *dst = p->osd_buf->map + rc.x0 * 4 + rc.y0 * p->osd_buf->stride;
+
+                 // Avoid overflowing if we have this special case.
+                 if (n == num_mod_rc - 1)
+                    --rh;
+
+                 memcpy_pic(dst, src, rw * 4, rh, p->osd_buf->stride, osd_sub->stride[0]);
             }
         }
     }
@@ -389,6 +437,19 @@ static void queue_flip(struct vo *vo, struct drm_frame *frame)
                     src_w << 16,
                     src_h << 16);
 
+    if (p->osd_changed) {
+        drmModeSetPlane(drm->fd, atomic_ctx->osd_plane->id, drm->crtc_id, p->osd_buf->id, 0,
+                        0,
+                        0,
+                        drm->fb->width,
+                        drm->fb->height,
+                        0,
+                        0,
+                        p->osd.w << 16,
+                        p->osd.h << 16);
+        p->osd_changed = false;
+    }
+
     int ret = drmModePageFlip(drm->fd, drm->crtc_id,
                               drm->fb->id, DRM_MODE_PAGE_FLIP_EVENT, drm);
     if (ret)
@@ -443,6 +504,10 @@ static void uninit(struct vo *vo)
 {
     struct priv *p = vo->priv;
 
+    if (p->osd_buf) {
+        destroy_framebuffer(p->osd_buf);
+    }
+
     if (!vo->hwdec && p->bufs) {
         for (int i = 0; i < p->buf_count; i++) {
             if (p->bufs[i]) {
@@ -496,6 +561,15 @@ static int preinit(struct vo *vo)
         goto err;
     }
 
+    if (!p->osd_buf) {
+        p->osd_buf = setup_framebuffer(vo, drm->mode.mode.hdisplay / 2, drm->mode.mode.vdisplay / 2, IMGFMT_ARGB);
+        if (!p->osd_buf) {
+            MP_ERR(vo, "Failed to allocate OSD buffer\n");
+            goto err;
+        }
+        p->osd_buf->locked = false;
+    }
+
     struct drm_atomic_context *atomic_ctx = drm->atomic_context;
 
     drmModeAtomicReqPtr request = drmModeAtomicAlloc();
@@ -504,15 +578,21 @@ static int preinit(struct vo *vo)
         goto err;
     }
 
-    ret = drm_object_set_property(request, atomic_ctx->drmprime_video_plane, "zorder", 1);
+    ret = drm_object_set_property(request, atomic_ctx->drmprime_video_plane, "zorder", 0);
     if (ret < 0) {
-        MP_ERR(drm, "Could not set ZPOS on prime plane: %s\n", mp_strerror(ret));
+        MP_ERR(drm, "Could not set ZPOS on video plane: %s\n", mp_strerror(ret));
         drmModeAtomicFree(request);
         goto err;
     }
-    ret = drm_object_set_property(request, atomic_ctx->draw_plane, "zorder", 0);
+    ret = drm_object_set_property(request, atomic_ctx->draw_plane, "zorder", 2);
     if (ret < 0) {
-        MP_ERR(drm, "Could not set ZPOS on video plane: %s\n", mp_strerror(ret));
+        MP_ERR(drm, "Could not set ZPOS on primary plane: %s\n", mp_strerror(ret));
+        drmModeAtomicFree(request);
+        goto err;
+    }
+    ret = drm_object_set_property(request, atomic_ctx->osd_plane, "zorder", 1);
+    if (ret < 0) {
+        MP_ERR(drm, "Could not set ZPOS on OSD plane: %s\n", mp_strerror(ret));
         drmModeAtomicFree(request);
         goto err;
     }
